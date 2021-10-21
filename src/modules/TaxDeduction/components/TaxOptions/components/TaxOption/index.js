cfedba6a23c165c3426c0321b6a200af9c2fb95d
Pavel <buteafulspace@gmail.com>
2021-10-22 00:17:50 +0400

    fix folder naming

--- src/modules/TaxDeduction/components/TaxOptions/components/TaxOption/index.js
+++ src/modules/TaxDeduction/components/TaxOptions/components/TaxOption/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import Checkbox from 'components/Checkbox/index';
+import Checkbox from 'components/Сheckbox/index';
 import './index.scss';
 
 const TaxOption = (props) => {
   const { name, value, index } = props;
-  <div className="taxOption">
-    <Checkbox name={name}>
-      <span className="taxOption__value">{value} рублей</span>
-      <span className="taxOption__info"> в {index}-ый год</span>
-    </Checkbox>
-  </div>;
+  return (
+    <div className="taxOption">
+      <Checkbox name={name}>
+        <span className="taxOption__value">{value} рублей</span>
+        <span className="taxOption__info"> в {index}-ый год</span>
+      </Checkbox>
+    </div>
+  );
 };
 
 export default TaxOption;
--- src/modules/TaxDeduction/components/TaxOptions/index.js
+++ src/modules/TaxDeduction/components/TaxOptions/index.js
@@ -3,16 +3,19 @@ import TaxOption from './components/TaxOption';
 import './index.scss';
 
 const TaxOptions = ({ options }) => {
-  <div className="taxOptions">
-    <span className="taxOptions__subTitle">
-      Итого можете внести <br /> в качестве досрочных:
-    </span>
-    <div className="taxOptions__checklist">
-      {options.map((option, i) => (
-        <TaxOption name={`option-${i}`} />
-      ))}
+  const test = 123;
+  return (
+    <div className="taxOptions">
+      <span className="taxOptions__subTitle">
+        Итого можете внести <br /> в качестве досрочных:
+      </span>
+      <div className="taxOptions__checklist">
+        {options.map((option, i) => (
+          <TaxOption name={`option-${i}`} />
+        ))}
+      </div>
     </div>
-  </div>;
+  );
 };
 
 export default TaxOptions;
