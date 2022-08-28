04e943283e61e6b66badb405c457b32cbc139861
keysee322 <info@asap-ag.ru>
2022-08-28 19:36:22 +0400

    middle-way

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import MyModal from './components/MyModal';
 import './App.css';
 
 function App() {
+  const [modal, setModal] = useState(false)
+  const closeModal = () => {
+    setModal(false)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        modal
+        ? <MyModal props={closeModal}/>
+        : <button onClick={()=>setModal(true)} className='button'>Налоговый вычет</button>
+      }
     </div>
   );
 }
